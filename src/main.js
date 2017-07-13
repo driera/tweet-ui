93e67158c2929d7a78370420f679d235aaa72b01
Dani Riera <info@driera.com>
2017-07-13 19:58:22 +0200

    uploading photos

--- src/main.js
+++ src/main.js
@@ -14,23 +14,42 @@ new Vue({
     el: '#twitterVue',
     data: {
         tweet: '',
-        message: message
+        message: message,
+        photo: null
     },
     computed: {
         tweetIsOutOfRange() {
             return this.charactersRemaining == MAX_TWEET_LENGTH || this.charactersRemaining < 0;
         },
-
         charactersRemaining() {
             return MAX_TWEET_LENGTH - this.tweet.length;
         },
-
         underTwentyMark() {
             return this.charactersRemaining <= 20 && this.charactersRemaining > 10;
         },
-
         underTenMark() {
             return this.charactersRemaining <= 10;
+        },
+        photoHasBeenUploaded() {
+            return this.photo !== null;
+        }
+    },
+    methods: {
+        triggerFileUpload() {
+            this.$refs.photoUpload.click();
+        },
+        handlePhotoUpload(e) {
+            let self = this;
+            let reader = new FileReader();
+
+            reader.onload = function(e) {
+                self.photo = e.target.result;
+            }
+
+            reader.readAsDataURL(e.target.files[0]);
+        },
+        removePhoto() {
+            this.photo = null;
         }
     }
 })
